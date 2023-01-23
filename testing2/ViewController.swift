d4df672e50aa5217e9b0a66398206bb685552806
KimRoks <rudfhr3161@naver.com>
2023-01-23 18:05:16 +0900

    Initial Commit

---
+++ testing2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testing2
+//
+//  Created by 김경록 on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
